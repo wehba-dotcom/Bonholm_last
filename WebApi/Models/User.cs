29cc9743d56cbd5fa4030ddfb7129971d8c0ff9d
Wehba Salem Korouni <wehkor@hotmail.com>
2024-05-29 14:05:34 +0200

    Begravelse Protokoller service is ready

--- WebApi/Models/User.cs
+++ WebApi/Models/User.cs
@@ -2,9 +2,9 @@
 {
     public class User
     {
-        public string ID { get; set; }
-        public string Email { get; set; }
-        public string Name { get; set; }
-        public string PhoneNumber { get; set; }
+        public string? ID { get; set; }
+        public string? Email { get; set; }
+        public string? Name { get; set; }
+        public string? PhoneNumber { get; set; }
     }
 }
